20b0e79a11bf64fd6632f7dd849b949c79da4ee8
Massimiliano Ziccardi <mziccard@redhat.com>
2019-11-10 23:28:35 +0100

    Added ability to register new constraint type

--- src/constraints/ConstraintFactory.ts
+++ src/constraints/ConstraintFactory.ts
@@ -4,13 +4,27 @@ import {
   FieldValueConstraint,
 } from './FieldValueConstraint';
 
+interface ConstraintDictionary {
+  [key: string]: (config: Config) => ConstraintInterface;
+}
+
 export class ConstraintFactory {
-  static create(config: Config): ConstraintInterface | null {
-    switch (config.type) {
-      case FIELD_VALUE_CONSTRAINT:
-        return new FieldValueConstraint(config);
-      default:
-        throw { message: `Unknown contraint type ${config.type}` };
+  private static constraints: ConstraintDictionary = {
+    FIELD_VALUE_CONSTRAINT: (config: Config) =>
+      new FieldValueConstraint(config),
+  };
+
+  static create(config: Config): ConstraintInterface {
+    if (ConstraintFactory.constraints[config.type as string]) {
+      return ConstraintFactory.constraints[config.type as string](config);
     }
+    throw { message: `Unknown constraint type ${config.type}` };
+  }
+
+  static register(
+    constraintType: string,
+    factory: (config: Config) => ConstraintInterface
+  ) {
+    this.constraints[constraintType] = factory;
   }
 }
