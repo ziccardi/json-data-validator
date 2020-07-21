37833c2278673ee1cb015bb00c2c5e6a088be53b
Massimiliano Ziccardi <mziccard@redhat.com>
2020-07-21 11:08:48 +0200

    fix(builder): fixed fluent api for the required rule builder
    The required rule builder was in need of 2 optional parameters: path and
    errorMessage.
    However errorMessage can be specified regardless whether
    path is present or not, making the api cumbersome.
    Changed the builder
    adding the withPath, withErrorMessage and build methods
    
    BREAKING CHANGE: Now the required rule builder requires the build method
    to be called

--- src/rules/builders/RequiredValueBuilder.ts
+++ src/rules/builders/RequiredValueBuilder.ts
@@ -1,9 +1,47 @@
 import {RuleConfig} from '../../config/RuleConfig';
 
+export interface RequiredRuleBuilder {
+  withPath: (path: string) => RequiredRuleBuilder;
+  withErrorMessage: (errorMessage: string) => RequiredRuleBuilder;
+  build: () => RuleConfig;
+}
+
+const createWithPath = (
+  rule: RuleConfig
+): ((path: string) => RequiredRuleBuilder) => {
+  const withPath = (path: string) => {
+    rule.path = path;
+    return {
+      withPath: createWithPath(rule),
+      withErrorMessage: createWithErrorMessage(rule),
+      build: createBuild(rule),
+    };
+  };
+
+  return withPath;
+};
+
+const createWithErrorMessage = (
+  rule: RuleConfig
+): ((path: string) => RequiredRuleBuilder) => {
+  const withErrorMessage = (errorMessage: string) => {
+    rule.errorMessage = errorMessage;
+    return {
+      withPath: createWithPath(rule),
+      withErrorMessage: createWithErrorMessage(rule),
+      build: createBuild(rule),
+    };
+  };
+
+  return withErrorMessage;
+};
+
+const createBuild = (rule: RuleConfig): (() => RuleConfig) => () => rule;
+
 export const builder = {
-  required: (path?: string, errorMessage?: string): RuleConfig => ({
-    type: 'REQUIRED',
-    path,
-    errorMessage,
-  }),
+  required: {
+    withPath: createWithPath({type: 'REQUIRED'}),
+    withErrorMessage: createWithErrorMessage({type: 'REQUIRED'}),
+    build: createBuild({type: 'REQUIRED'}),
+  },
 };
