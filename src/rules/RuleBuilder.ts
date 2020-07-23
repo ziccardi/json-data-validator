05bd51ce2d4eb1f8979bf38ce9f3d0f876f03b62
Massimiliano Ziccardi <mziccard@redhat.com>
2020-07-23 18:45:44 +0200

    feat(builder): added IsBase64Builder

--- src/rules/RuleBuilder.ts
+++ src/rules/RuleBuilder.ts
@@ -4,6 +4,7 @@ import {builder as CompositeRuleBuilder} from './builders/CompositeRuleBuilder';
 import {builder as RequiredRuleBuilder} from './builders/RequiredValueBuilder';
 import {builder as LengthRuleBuilder} from './builders/LengthRuleBuilder';
 import {builder as MatchesRuleBuilder} from './builders/MatchesRuleBuilder';
+import {builder as IsBase64Builder} from './builders/IsBase64Builder';
 
 // tslint:disable-next-line:variable-name
 export const RuleBuilder = {
@@ -13,4 +14,5 @@ export const RuleBuilder = {
   required: RequiredRuleBuilder.required,
   length: LengthRuleBuilder,
   matches: MatchesRuleBuilder.withPattern,
+  isBase64: IsBase64Builder,
 };
