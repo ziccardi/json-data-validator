923a8d782393c45f78da88a7e724b8b6b4b14721
Massimiliano Ziccardi <mziccard@redhat.com>
2021-01-07 18:16:37 +0100

    fix: 🐛 uniformed the builder interface to the other builders
    Now even if this builder configures only one property, it is needed to
    call the `build` method to actually build the rule
    
    BREAKING CHANGE: 🧨 Now even if this builder configures only one property, it is needed to
    call the `build` method to actually build the rule

--- src/rules/builders/ExactValueBuilder.ts
+++ src/rules/builders/ExactValueBuilder.ts
@@ -1,19 +1,37 @@
 import {RuleConfig} from '../../config/RuleConfig';
 
+interface ExactValueRuleConfig extends RuleConfig {
+  type: 'EXACT_VALUE';
+  value: string | number;
+}
+
+export class Builder {
+  private readonly cfg: ExactValueRuleConfig;
+  constructor(cfg: ExactValueRuleConfig) {
+    this.cfg = cfg;
+  }
+
+  public build(): ExactValueRuleConfig {
+    return this.cfg;
+  }
+}
+
 export const builder = {
-  withValue: (value: string | number, errorMessage?: string): RuleConfig => ({
-    type: 'EXACT_VALUE',
-    value,
-    errorMessage,
-  }),
+  withValue: (value: string | number, errorMessage?: string): Builder =>
+    new Builder({
+      type: 'EXACT_VALUE',
+      value,
+      errorMessage,
+    }),
   withPathAndValue: (
     path: string,
     value: string | number,
     errorMessage?: string
-  ): RuleConfig => ({
-    type: 'EXACT_VALUE',
-    path,
-    value,
-    errorMessage,
-  }),
+  ): Builder =>
+    new Builder({
+      type: 'EXACT_VALUE',
+      path,
+      value,
+      errorMessage,
+    }),
 };
