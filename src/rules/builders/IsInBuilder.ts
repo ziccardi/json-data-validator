e7ec6522fb3a939985371f1187bcc5628449191e
Massimiliano Ziccardi <mziccard@redhat.com>
2021-01-07 18:17:34 +0100

    refactor: 💡 cleaner interface
    Refactored with more readable code

--- src/rules/builders/IsInBuilder.ts
+++ src/rules/builders/IsInBuilder.ts
@@ -5,80 +5,36 @@ export interface IsInBuilderInterface {
   withValue(value: string): IsInBuilderFinalInterface;
 }
 
-export interface IsInBuilderFinalInterface {
-  withValues(...values: string[]): IsInBuilderFinalInterface;
-  withValue(value: string): IsInBuilderFinalInterface;
+export interface IsInBuilderFinalInterface extends IsInBuilderInterface {
   build(errorMessage?: string): RuleConfig;
 }
 
-function createWithValueFunction(
-  rule: RuleConfig
-): (value: string) => IsInBuilderFinalInterface {
-  const withValue = (value: string): IsInBuilderFinalInterface => {
-    (rule.values as string[]).push(value);
-
-    return {
-      withValues: createWithValuesFunction(rule),
-      withValue: createWithValueFunction(rule),
-      build: createBuildFunction(rule),
-    };
-  };
-  return withValue;
+interface IsInBuilderConfig extends RuleConfig {
+  type: 'isIn';
+  values: string[];
 }
 
-function createWithValuesFunction(
-  rule: RuleConfig
-): (value: string) => IsInBuilderFinalInterface {
-  const withValues = (...values: string[]): IsInBuilderFinalInterface => {
-    (rule.values as string[]) = [...(rule.values as string[]), ...values];
-
-    return {
-      withValues: createWithValuesFunction(rule),
-      withValue: createWithValueFunction(rule),
-      build: createBuildFunction(rule),
-    };
+class IsInBuilder implements IsInBuilderFinalInterface {
+  private readonly cfg: IsInBuilderConfig = {
+    type: 'isIn',
+    values: [],
   };
 
-  return withValues;
-}
-
-function createBuildFunction(
-  rule: RuleConfig
-): (errorMessage: string) => RuleConfig {
-  const build = (errorMessage?: string) => {
-    return {
-      type: rule.type,
-      values: (rule.values as string[]).join(),
-      errorMessage,
-    };
+  public readonly withValue = (value: string): IsInBuilderFinalInterface => {
+    this.cfg.values.push(value);
+    return this;
   };
 
-  return build;
+  public readonly withValues = (
+    ...values: string[]
+  ): IsInBuilderFinalInterface => {
+    this.cfg.values = [...this.cfg.values, ...values];
+    return this;
+  };
+
+  public build(errorMessage?: string) {
+    return {...this.cfg, values: this.cfg.values.join(','), errorMessage};
+  }
 }
 
-export const builder: IsInBuilderInterface = {
-  withValues: (...values: string[]): IsInBuilderFinalInterface => {
-    const rule: RuleConfig = {
-      type: 'isIn',
-      values: values || [],
-    };
-
-    return {
-      withValues: createWithValuesFunction(rule),
-      withValue: createWithValueFunction(rule),
-      build: createBuildFunction(rule),
-    };
-  },
-  withValue: (value: string): IsInBuilderFinalInterface => {
-    const rule: RuleConfig = {
-      type: 'isIn',
-      values: [value],
-    };
-
-    return {
-      withValues: createWithValuesFunction(rule),
-      withValue: createWithValueFunction(rule),
-      build: createBuildFunction(rule),
-    };
-  },
-};
+export const builder: IsInBuilderInterface = new IsInBuilder() as IsInBuilderInterface;
