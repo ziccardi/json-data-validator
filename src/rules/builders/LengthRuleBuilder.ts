afa724dd428224273c1ba8b166070a96f68cbf66
Massimiliano Ziccardi <mziccard@redhat.com>
2021-01-07 18:18:45 +0100

    fix: 🐛 added build method
    added build method to uniform the builder to the others
    
    BREAKING CHANGE: 🧨 Now you need to call the `build` method to actually build the rule

--- src/rules/builders/LengthRuleBuilder.ts
+++ src/rules/builders/LengthRuleBuilder.ts
@@ -1,10 +1,19 @@
-import * as LengthRule from '../LengthRule';
 import {RuleConfig} from '../../config/RuleConfig';
 
+export interface LengthRuleConfig extends RuleConfig {
+  type: 'LENGTH';
+  length: number;
+}
+
 export const builder = {
-  withLength: (length: number, errorMessage?: string): RuleConfig => ({
-    type: LengthRule.NAME,
-    length,
-    errorMessage,
-  }),
+  withLength: (length: number, errorMessage?: string) => {
+    const cfg: LengthRuleConfig = {
+      type: 'LENGTH',
+      length,
+      errorMessage,
+    };
+    return {
+      build: () => cfg,
+    };
+  },
 };
